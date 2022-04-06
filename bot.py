19184f57d2d9de45ff3c5627b2060b5b6e4464ac
Pupi65 <100903523+Pupi65@users.noreply.github.com>
2022-04-05 22:58:58 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -42,7 +42,7 @@ def main():
     api_id = 7386053
     api_hash = '78d1c032f3aa546ff5176d9ff0e7f341'
     bot_token = '5223817057:AAHWtUx6ZIfdMwnR1UxA7TBPKCyd5Ty10hw'
-    tl_admin_user = 'naruto0109'
+    tl_admin_user = 'nautaii'
     # end
 
     #create dispatcher and regs
